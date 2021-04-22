e59c9fa94b4048612297884a983f58de8f9e2050
bellehlim <53798540+bellehlim@users.noreply.github.com>
2021-04-21 22:51:45 -0400

    Update MathMagicMixer_v4.py

--- MathMagicMixer_v4.py
+++ MathMagicMixer_v4.py
@@ -1,5 +1,4 @@
 from z3 import *
-s = SolverFor("LIA")
 
 ops = [ "*", "-", "+", "/" ]
 
@@ -7,51 +6,68 @@ ops = [ "*", "-", "+", "/" ]
 def oneOf(x, l):
     return Or([ x == p for p in l ])
 
-def secondTerm(target, a, o1, b):
+#checks if the given _target_ value is attainable with a binary operation between _a_ and _b_
+def binOp(target, a, o1, b):
     a, b = Reals('a b')
-    #print("secondTerm")
     return Or(And(o1 == '*', a * b == target),
               And(o1 == '/', a / b == target),
-              And(o1 == '+', a + b == target),
-              And(o1 == '-', a - b == target))
+              And(o1 == '-', a - b == target),
+              And(o1 == '+', a + b == target))
 
-def thirdTerm(target, a, o1, b, o2, c):
+#checks if the given _target_ value is attainable with binary operations between (_a_ and _b_) and _c_
+def triOp(target, a, o1, b, o2, c):
     r1, r2 = Reals('r1 r2')
     r1 = target
     r2 = c
-    #print("thirdTerm")
-    return Or(And(o2 == '*', secondTerm(r1 / r2, a, o1, b)),
-              And(o2 == '/', secondTerm(r1 * r2, a, o1, b)),
-              And(o2 == '+', secondTerm(r1 + r2, a, o1, b)),
-              And(o2 == '-', secondTerm(r1 - r2, a, o1, b)))
+    return Or(And(o2 == '*', binOp(r1 / r2, a, o1, b)),
+              And(o2 == '/', binOp(r1 * r2, a, o1, b)),
+              And(o2 == '-', binOp(r1 + r2, a, o1, b)),
+              And(o2 == '+', binOp(r1 - r2, a, o1, b)))
 
-def fourthTerm(target, a, o1, b, o2, c, o3, d):
+#checks if the given _target_ value is attainable with binary operations between ((_a_ and _b_) and _c_) and _d_
+def quadOp(target, a, o1, b, o2, c, o3, d):
     r1, r2 = Reals('r1 r2')
     r1 = target
     r2 = d
-    #print("fourthTerm")
-    return Or(And(o3 == '*', thirdTerm(r1 / r2, a, o1, b, o2, c)),
-              And(o3 == '/', thirdTerm(r1 * r2, a, o1, b, o2, c)),
-              And(o3 == '+', thirdTerm(r1 + r2, a, o1, b, o2, c)),
-              And(o3 == '-', thirdTerm(r1 - r2, a, o1, b, o2, c)))
+    return Or(And(o3 == '*', triOp(r1 / r2, a, o1, b, o2, c)),
+              And(o3 == '/', triOp(r1 * r2, a, o1, b, o2, c)),
+              And(o3 == '-', triOp(r1 + r2, a, o1, b, o2, c)),
+              And(o3 == '+', triOp(r1 - r2, a, o1, b, o2, c)))
 
-def fifthTerm(target, a, o1, b, o2, c, o3, d, o4, e):
+#checks if the given _target_ value is attainable with binary operations between (((_a_ and _b_) and _c_) and _d_) and _e_
+def pentaOp(target, a, o1, b, o2, c, o3, d, o4, e):
     r1, r2 = Reals('r1 r2')
     r1 = target
     r2 = e
-    #print("fifthTerm")
-    return Or(And(o4 == '*', fourthTerm(r1 / r2, a, o1, b, o2, c, o3, d)),
-              And(o4 == '/', fourthTerm(r1 * r2, a, o1, b, o2, c, o3, d)),
-              And(o4 == '+', fourthTerm(r1 + r2, a, o1, b, o2, c, o3, d)),
-              And(o4 == '-', fourthTerm(r1 - r2, a, o1, b, o2, c, o3, d)))
+    return Or(And(o4 == '*', quadOp(r1 / r2, a, o1, b, o2, c, o3, d)),
+              And(o4 == '/', quadOp(r1 * r2, a, o1, b, o2, c, o3, d)),
+              And(o4 == '-', quadOp(r1 + r2, a, o1, b, o2, c, o3, d)),
+              And(o4 == '+', quadOp(r1 - r2, a, o1, b, o2, c, o3, d)))
+
+#returns the list with _a_ removed (once) if present
+def removeIfPresent(a, lst):
+    if a in lst:
+        lst.remove(a)
+        return lst
+    else:
+        return lst
+
+#checks each of the five items matches to 1 instance in the given list
+def our_distinct(a, b, c, d, e, given):
+    z = And(a in given,
+               b in removeIfPresent(a, given),
+               c in removeIfPresent(b, removeIfPresent(a, given)),
+               d in removeIfPresent(c, removeIfPresent(b, removeIfPresent(a, given))),
+               e in removeIfPresent(d, removeIfPresent(c, removeIfPresent(b, removeIfPresent(a, given)))))
+    print(z)
+    return z
 
 #main -- provide it the target number of list of 5 other numbers          
 def findSolutions(target, given):
     a, b, c, d, e = Reals('a b c d e')
     o1, o2, o3, o4 = Strings('o1 o2 o3 o4')
-    n1, n2, n3, n4, n5 = Strings('n1 n2 n3 n4 n5')
     t = Solver()
-    t.add(fifthTerm(target, a, o1, b, o2, c, o3, d, o4, e),
+    t.add(pentaOp(target, a, o1, b, o2, c, o3, d, o4, e),
           oneOf(a, given),
           oneOf(b, given),
           oneOf(c, given),
@@ -62,18 +78,19 @@ def findSolutions(target, given):
           oneOf(o3, ops),
           oneOf(o4, ops),
           Distinct(a, b, c, d, e))
-    print("finished round")
+          #our_distinct(a, b, c, d, e, given))
     while t.check() == sat:
-        n1 = str(t.model()[a].as_long())
-        n2 = str(t.model()[b].as_long())
-        n3 = str(t.model()[c].as_long())
-        n4 = str(t.model()[d].as_long())
-        n5 = str(t.model()[e].as_long())
+        n1 = t.model()[a]
+        n2 = t.model()[b]
+        n3 = t.model()[c]
+        n4 = t.model()[d]
+        n5 = t.model()[e]
         op1 = as_string(t.model()[o1])
         op2 = as_string(t.model()[o2])
         op3 = as_string(t.model()[o3])
         op4 = as_string(t.model()[o4])
-        print (n1, op1, n2, op2, n3, op3, n4, op4, n5)
+        print ("((((", n1, op1, n2, ")", op2, n3, ")", op3, n4, ")", op4, n5, ")")
+        #print ("((((", t.model()[a].as_long(), op1, t.model()[b].as_long(), ")", op2, t.model()[c].as_long(), ")", op3, t.model()[d].as_long(), ")", op4, t.model()[d].as_long(), ")")
         t.add(Not(And(a == t.model()[a],
                       b == t.model()[b],
                       c == t.model()[c],
@@ -84,8 +101,8 @@ def findSolutions(target, given):
                       o3 == t.model()[o3],
                       o4 == t.model()[o4])))
     print("done")
-        
-    
+
+#removes quotation marks from accessed string item in array when printing (ex. instead of printing (3 "*" 2), print (3 * 2))     
 def as_string(self):
          """Return a string representation of sequence expression."""
          if self.is_string_value():
